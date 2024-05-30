5c361c89d722f85632c8609721ccf77e6bb925d3
jeniferocha <jenifermartinsdarocha@hotmail.com>
2024-05-30 20:11:50 -0300

    atualizacao carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,17 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const carrinhoLista = document.getElementById("carrinho-lista");
   const limparCarrinhoButton = document.getElementById("limpar-carrinho");
+  const totalPrecoElement = document.getElementById("total-preco");
 
   let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  let totalPreco = 0;
 
   carrinho.forEach((produto) => {
     const listItem = document.createElement("li");
     listItem.innerHTML = `
-      <img src="${produto.imagem}" alt="${produto.nome}" style="width: 50px; height: 50px;">
+      <img src="${produto.imagem}" alt="${produto.nome}"">
       ${produto.nome} - ${produto.preco}
     `;
     carrinhoLista.appendChild(listItem);
+    const preco = parseFloat(produto.preco.replace("R$", "").replace(",", "."));
+    totalPreco += preco;
   });
+  totalPrecoElement.textContent = `Total: R$ ${totalPreco.toFixed(2).replace(".", ",")}`;
 
   limparCarrinhoButton.addEventListener("click", () => {
     localStorage.removeItem("carrinho");
