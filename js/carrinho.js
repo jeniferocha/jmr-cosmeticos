fc5fab1d246f3e8e8dcfde80c71b9deb02c0dc43
jeniferocha <jenifermartinsdarocha@hotmail.com>
2024-07-29 11:06:57 -0300

    instruções readme

--- js/carrinho.js
+++ js/carrinho.js
@@ -6,23 +6,52 @@ document.addEventListener("DOMContentLoaded", () => {
   let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
   let totalPreco = 0;
 
-  carrinho.forEach((produto) => {
+  let userLogado = JSON.parse(localStorage.getItem("userLogado"));
+  if (userLogado) {
+    carrinho = carrinho.map((produto) => {
+      if (!produto.clienteId || !produto.clienteNome) {
+        return {
+          ...produto,
+          clienteId: userLogado._id,
+          clienteNome: userLogado.name,
+        };
+      }
+      return produto;
+    });
+
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  }
+
+  carrinho.forEach((item) => {
     const listItem = document.createElement("li");
     listItem.innerHTML = `
-      <img src="${produto.imagem}" alt="${produto.nome}"">
-      ${produto.nome} - ${produto.preco}
+      <img src="${item.imagem}" alt="${item.nome}">
+      ${item.nome} - ${item.preco}
+    
     `;
     carrinhoLista.appendChild(listItem);
-    const preco = parseFloat(produto.preco.replace("R$", "").replace(",", "."));
+    const preco = parseFloat(item.preco.replace("R$", "").replace(",", "."));
     totalPreco += preco;
   });
   totalPrecoElement.textContent = `Total: R$ ${totalPreco.toFixed(2).replace(".", ",")}`;
 
   limparCarrinhoButton.addEventListener("click", () => {
     localStorage.removeItem("carrinho");
-    totalPrecoElement.textContent = 'Total: R$ 0,00';
+    totalPrecoElement.textContent = "Total: R$ 0,00";
     while (carrinhoLista.firstChild) {
       carrinhoLista.removeChild(carrinhoLista.firstChild);
     }
   });
 });
+
+function usuarioLogado() {
+  let userLogado = JSON.parse(localStorage.getItem("userLogado"));
+  if (userLogado) {
+    const usuarioLogado = document.querySelector(".user_logado");
+    usuarioLogado.innerHTML = ` 
+              <label class="user_logado">Olá, ${userLogado.name}</label>        
+          `;
+  }
+}
+
+usuarioLogado();
